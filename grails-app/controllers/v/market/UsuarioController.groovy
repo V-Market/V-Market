50aa06f2c329eb5f06fbe47ca75a4317e4acccae
Neill <neillro24124@gmail.com>
2015-04-21 19:41:43 -0500

    UsuarioController, No se por que no subio

--- grails-app/controllers/v/market/UsuarioController.groovy
+++ grails-app/controllers/v/market/UsuarioController.groovy
@@ -7,30 +7,123 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class UsuarioController {
 
-    def index =  {
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Usuario.list(params), model: [usuarioInstanceCount: Usuario.count()]
     }
 
     def show(Usuario usuarioInstance) {
+        respond usuarioInstance
     }
 
     def create() {
+        respond new Usuario(params)
     }
 
     @Transactional
     def save(Usuario usuarioInstance) {
+        if (usuarioInstance == null) {
+            notFound()
+            return
+        }
+
+        if (usuarioInstance.hasErrors()) {
+            respond usuarioInstance.errors, view: 'create'
+            return
+        }
+
+        usuarioInstance.save flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'usuario.label', default: 'Usuario'), usuarioInstance.id])
+                redirect usuarioInstance
+            }
+            '*' { respond usuarioInstance, [status: CREATED] }
+        }
     }
 
     def edit(Usuario usuarioInstance) {
+        respond usuarioInstance
     }
 
     @Transactional
     def update(Usuario usuarioInstance) {
+        if (usuarioInstance == null) {
+            notFound()
+            return
+        }
+
+        if (usuarioInstance.hasErrors()) {
+            respond usuarioInstance.errors, view: 'edit'
+            return
+        }
+
+        usuarioInstance.save flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Usuario.label', default: 'Usuario'), usuarioInstance.id])
+                redirect usuarioInstance
+            }
+            '*' { respond usuarioInstance, [status: OK] }
+        }
     }
 
     @Transactional
     def delete(Usuario usuarioInstance) {
+
+        if (usuarioInstance == null) {
+            notFound()
+            return
+        }
+
+        usuarioInstance.delete flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Usuario.label', default: 'Usuario'), usuarioInstance.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NO_CONTENT }
+        }
     }
 
     protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'usuario.label', default: 'Usuario'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NOT_FOUND }
+        }
+    }
+
+    def profile(){
+
+    }
+
+    def authenticate(){
+        if(request.method=="POST") {
+            def user = Usuario.findByUserNameAndPassword(params.login , params.password);
+            //def user = Usuario.findbyLoginAndPassword(params.login, params.password);
+            if (user) {
+                session.user = user;
+                redirect(controller: "usuario", action: "profile")
+                //redirect(controller: params.controllerPre , view: params.actionPre)
+            } else {
+                //flash.message = "Sorry, ${params.login}. Please try again"
+                session.user = null
+                redirect(controller: "usuario", action: "profile")
+            }
+        }
+    }
+
+    def logout(){
+        session.user = null
+        redirect(controller: "usuario", action: "profile" )
+        //redirect(controller: params.controllerPre , view: params.actionPre)
     }
 }
