509c99869a2c586c9d024e4e5aff969fb1b1aa31
Neill <neillro24124@gmail.com>
2015-06-07 17:21:58 -0500

    Index 80%

--- grails-app/controllers/v/market/AuthController.groovy
+++ grails-app/controllers/v/market/AuthController.groovy
@@ -1,10 +1,22 @@
 package v.market
 
+
 class AuthController {
 
     //static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index() {
+
+        //[categories:['Salud y Aseo','Licores','Refrigerados','Frutas y Verduras', 'Alimentos varios']]
+        def cate = [[1:'saludyaseo',2:'Salud y Aseo'],[1:'licores',2:'Licores'],[1:'refrigerados',2:'Refrigerados'],[1:'frutasyverduras',2:'Frutas y Verduras'],[1:'alimentosvarios',2:'Alimentos Varios']]
+        def cat = new String("'#saludyaseo','#licores','#refrigerados','#frutasyverduras','#alimentosvarios'").encodeAsRaw();
+        //def cat = new String("'#refrigerados','#carnes','#fritos'").encodeAsRaw();
+        //def cate = ['Refrigerados','Carnes','Fritos'];
+        //def divs = ['#Refrigerados','#Carnes','#Fritos'];
+        //def cate = ['Salud y Aseo','Licores','Refrigerados','Frutas y Verduras', 'Alimentos varios']
+        def divs = ['#saludyaseo','#licores','#refrigerados','#frutasyverduras','#alimentosvarios']
+        [categories: cat,cate:cate];
+
     }
 
     def doLogin(){
